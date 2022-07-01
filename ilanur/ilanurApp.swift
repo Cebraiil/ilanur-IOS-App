8134e54ba7b422629e621060a66b47e7025097c4
Dev Light <106925514+developerlight@users.noreply.github.com>
2022-07-01 19:15:17 +0300

    Initial Commit

---
+++ ilanur/ilanurApp.swift
@@ -0,0 +1,17 @@
+//
+//  ilanurApp.swift
+//  ilanur
+//
+//  Created by Developer Light on 1.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ilanurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
